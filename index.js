793bd4354adc7fea8d482d87fd59bccfc211a074
tunnelrat16 <tjandmatt@hotmail.com>
2022-04-20 21:59:12 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function displayPlan(){
-  const step1 = "Collect underpants."
-  const step2 = "???"
-  const step3 = "Profit!"
+const li1 = document.querySelector('#step-1')
+const li2 = document.querySelector('#step-2')
+const li3 = document.querySelector('#step-3')
+
+function displayPlan() {
+    const step1 = "Collect underpants."
+    const step2 = "???"
+    const step3 = "Profit!"
+    li1.innerHTML = `${step1}`;
+    li2.innerHTML = `${step2}`;
+    li3.innerHTML = `${step3}`;
 }
